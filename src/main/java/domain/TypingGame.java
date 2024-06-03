da513949aef4cc57e10267b59b509058abf619fc
ffeusthuber <ffeusthu@gmail.com>
2024-06-03 14:39:03 +0200

    moved setup of wordSpawner to static build methode

--- src/main/java/domain/TypingGame.java
+++ src/main/java/domain/TypingGame.java
@@ -1,54 +1,34 @@
 package domain;
 
-import adapter.in.TextFileWordRepository;
-import adapter.in.WordRepository;
 import adapter.out.ConsoleDisplay;
-import adapter.out.DisplayAdapter;
-import util.RandomNumberGenerator;
-import util.RandomNumbers;
-
-import java.util.List;
+import application.TypingGameApplication;
+import domain.port.out.DisplayPort;
 
 public class TypingGame {
     private static final int INITIAL_PLAYER_LIVES = 3;
 
     protected int playerLives;
     private WordSpawner wordSpawner;
-    private DisplayAdapter display;
+    private DisplayPort display;
+    private TaskManager taskManager;
 
 
     public TypingGame(){
         init();
     }
 
-    private void init(){
-        this.playerLives = INITIAL_PLAYER_LIVES;
-        this.wordSpawner = setupWordSpawner();
-        this.display = new ConsoleDisplay();
+
+    public TypingGame(TypingGameApplication typingGameApplication) {
+        init();
     }
 
-    private WordSpawner setupWordSpawner() {
-        WordRepository wordRepository = new TextFileWordRepository("src/main/java/config/wordList.txt");
-        RandomNumbers randomNumberGenerator = new RandomNumberGenerator();
-        StringSelector stringSelector = new StringSelectorImpl(wordRepository, randomNumberGenerator);
-        SpawnPointSelector spawnPointSelector = new SpawnPointSelectorImpl(randomNumberGenerator,
-                                                                           new Position(1,10),
-                                                                           new Position(2,10),
-                                                                           new Position(3,10));
-
-        return new WordSpawner(stringSelector, spawnPointSelector);
+    private void init(){
+        this.playerLives = INITIAL_PLAYER_LIVES;
+        this.display = new ConsoleDisplay();
+        this.wordSpawner = WordSpawner.build();
     }
 
     public void start() {
-        Word word = wordSpawner.spawnOnRandomSpawnPoint();
-        display.display(List.of(word));
-    }
-
-    public WordSpawner getWordSpawner() {
-        return this.wordSpawner;
-    }
-
-    public DisplayAdapter getDisplay() {
-        return this.display;
+        taskManager.runTimedTasks();
     }
 }
--- src/main/java/domain/WordSpawner.java
+++ src/main/java/domain/WordSpawner.java
@@ -1,6 +1,11 @@
 package domain;
 
-public class WordSpawner {
+import adapter.out.TextFileWordRepository;
+import domain.port.out.WordRepository;
+import util.RandomNumberGenerator;
+import util.RandomNumbers;
+
+public class WordSpawner implements Runnable{
     private final StringSelector stringSelector;
     private final SpawnPointSelector spawnPointSelector;
 
@@ -12,5 +17,22 @@ public class WordSpawner {
 
     public Word spawnOnRandomSpawnPoint() {
         return new Word(stringSelector.random(),spawnPointSelector.random());
+    }
+
+    @Override
+    public void run() {
+        spawnOnRandomSpawnPoint();
+    }
+
+    public static WordSpawner build() {
+        WordRepository wordRepository = new TextFileWordRepository("src/main/java/config/wordList.txt");
+        RandomNumbers randomNumberGenerator = new RandomNumberGenerator();
+        StringSelector stringSelector = new StringSelectorImpl(wordRepository, randomNumberGenerator);
+        SpawnPointSelector spawnPointSelector = new SpawnPointSelectorImpl(randomNumberGenerator,
+                                                                           new Position(1,10),
+                                                                           new Position(2,10),
+                                                                           new Position(3,10));
+
+        return new WordSpawner(stringSelector, spawnPointSelector);
     }
 }

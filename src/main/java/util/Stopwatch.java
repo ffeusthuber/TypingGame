4ebf8f7634864e52aebaa542580e365fc0c95c6d
ffeusthuber <ffeusthu@gmail.com>
2024-09-04 11:37:00 +0200

    added Stopwatch

---
+++ src/main/java/util/Stopwatch.java
@@ -0,0 +1,35 @@
+package util;
+
+import java.time.Clock;
+import java.time.Duration;
+import java.time.Instant;
+import java.time.temporal.TemporalAmount;
+
+public class Stopwatch {
+    private final Clock clock;
+    private TemporalAmount elapsedTime = Duration.ZERO;
+    private Instant startInstant;
+    boolean isRunning = false;
+
+    public Stopwatch(Clock clock) {
+        this.clock = clock;
+    }
+
+    public void start() {
+        startInstant = clock.instant();
+        isRunning = true;
+    }
+
+    public void stop() {
+        Instant stopInstant = clock.instant();
+        isRunning = false;
+        this.elapsedTime = Duration.between(startInstant, stopInstant);
+    }
+
+    public TemporalAmount getElapsedTime() {
+        if(isRunning) {
+            return Duration.between(startInstant, clock.instant());
+        }
+        return this.elapsedTime;
+    }
+}
---
+++ src/test/java/util/StopwatchTest.java
@@ -0,0 +1,25 @@
+package util;
+
+import org.junit.jupiter.api.Test;
+
+import java.time.Clock;
+import java.time.Instant;
+import java.time.temporal.TemporalAmount;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+public class StopwatchTest {
+
+    @Test
+    void testStopwatch() {
+        MutableClock clock = new MutableClock(Instant.EPOCH, Clock.systemDefaultZone().getZone());
+        Stopwatch stopwatch = new Stopwatch(clock);
+        TemporalAmount runTime = java.time.Duration.ofSeconds(10);
+
+        stopwatch.start();
+        clock.fastForward(runTime);
+        stopwatch.stop();
+
+        assertEquals(runTime, stopwatch.getElapsedTime());
+    }
+}
